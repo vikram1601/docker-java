631574f607b2e8a4c5eb76dc039bcae6bf03dd1f
Vikram Singh <vikram@devtron.ai>
2020-07-09 00:09:03 +0530

    test added

---
+++ src/test/java/com/mkyong/StartApplicationTest.java
@@ -0,0 +1,16 @@
+package com.mkyong;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StartApplicationTest {
+
+    private String INPUT = "123456";
+
+    @Test
+    public void testHex() {
+        String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        System.out.println(expected);
+         System.out.println("SAMPLE TEST IS PAASSED HERE");
+    }
+}
